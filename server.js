de7a8b6c178f0d412c1ea0a6befcec3e322386cb
Nhan nguyen <trongnhan202033@email.com>
2023-08-03 13:54:08 +0700

    video5

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-const http = require('http')
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send("hello world")
+})
+
+app.get('/about', (req, res) => {
+    res.send("hello About")
+})
+
+app.listen(port, () => {
+    console.log(`heelo ${port}`)
+})
